8e8dfde2de272b4198384cf20e5feb87bc5398e2
naomi-tan <69682497+naomi-tan@users.noreply.github.com>
2024-02-22 19:46:01 +0000

    feat: add buttons and display to page

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+// Define arithmetic operations
+
 function add(a, b){
     return a + b;
 }
@@ -27,6 +29,83 @@ function operate(n1, n2, op){
     }
 }
 
-let n1 = 0;
-let n2 = 0;
-let op = '';
+let n1 = undefined;
+let n2 = undefined;
+let op = undefined;
+
+// Get display and button containers to update
+
+let display = document.querySelector('.display');
+let buttons = document.querySelector('.buttons');
+
+// Define button text
+
+button_text = [
+    ['+', '-', 'x', '/'],
+    ['7', '8', '9', 'Clear'],
+    ['4', '5', '6', 'Backspace'],
+    ['1', '2', '3', 'Power'],
+    ['+/-', '0', '.', '=']
+]
+
+// Populate buttons and text
+let i = 0;
+for(let row of button_text){
+    let j = 0;
+    let row_element = document.createElement('div');
+    row_element.className = 'row';
+    row_element.id = `row${i}`;
+    for(let text of row){
+        let button = document.createElement('button');
+        button.textContent = text;
+        button.className = 'button';
+        button.id = `button${i}${j}`;
+        row_element.appendChild(button);
+        j++;
+    }
+    buttons.appendChild(row_element);
+    i++;
+}
+
+// Button press functions
+
+let update_text;
+
+buttons.addEventListener('click', (event) => {
+    let display_text = display.textContent;
+    // if operator pressed store n1 and operator
+    // if = pressed store n2 and operate
+    console.log(event.target.tagName)
+    if(event.target.tagName == 'BUTTON'){
+        switch(event.target.textContent){
+            // soemtimes writes all buttons text content
+            case 'Clear':
+                update_text = '';
+                break;
+            case 'Backspace':
+                if(display_text.length == 0){
+                    break;
+                }
+                else {
+                    update_text = display_text.slice(0, display_text.length - 1);
+                    break;
+                }
+            case '.': // 1 . per number not per display
+                if(display_text.includes('.')) {
+                    break;
+                }
+                else {
+                    update_text = display_text + event.target.textContent;
+                }
+            default: update_text = display_text + event.target.textContent;
+        }
+    }
+
+    display.textContent = update_text;
+    // display.textContent = display_text + event.target.textContent;
+})
+
+// what if multiple operators? BODMAS
+
+// if row =0 or col = 4
+// if . in display dont append
