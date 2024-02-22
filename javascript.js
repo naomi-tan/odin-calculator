c406c63223f689a9eae6154b962eb0882b9af734
naomi-tan <69682497+naomi-tan@users.noreply.github.com>
2024-02-22 20:21:56 +0000

    feat: assign n1 and op when operator pressed

--- javascript.js
+++ javascript.js
@@ -16,16 +16,22 @@ function divide(a, b) {
     return a / b;
 }
 
+function power(a, b){
+    return a ** b;
+}
+
 function operate(n1, n2, op){
     switch(op){
         case '+':
             return add(n1, n2);
         case '-':
             return subtract(n1, n2);
-        case '*':
+        case 'x':
             return multiply(n1, n2);
         case '/':
             return divide(n1, n2);
+        case 'x^n':
+            return power(n1, n2);
     }
 }
 
@@ -44,7 +50,7 @@ button_text = [
     ['+', '-', 'x', '/'],
     ['7', '8', '9', 'Clear'],
     ['4', '5', '6', 'Backspace'],
-    ['1', '2', '3', 'Power'],
+    ['1', '2', '3', 'x^n'],
     ['+/-', '0', '.', '=']
 ]
 
@@ -78,11 +84,38 @@ buttons.addEventListener('click', (event) => {
     console.log(event.target.tagName)
     if(event.target.tagName == 'BUTTON'){
         switch(event.target.textContent){
-            // soemtimes writes all buttons text content
             case 'Clear':
                 update_text = '';
+                n1 = undefined;
+                n2 = undefined;
+                op = undefined;
                 break;
-            case 'Backspace':
+            // sometimes writes all buttons text content
+            case '+':
+            case '-':
+            case 'x':
+            case '/':
+            case 'x^n':
+                if(display_text == ""){ // cannot start with operator
+                    break;
+                }
+                else if(op == undefined){ // only allow 1 operator per sum
+                    op = event.target.textContent;
+                    n1 = Number(display_text.replace('+', ''));
+                    console.log(op)
+                    console.log(n1)
+                    update_text = display_text + event.target.textContent;
+                    break;
+                }
+                else{
+                    break;
+                }
+            case '=':
+                // n1, op and n2 must exist otherwise do nothing
+                if((n1 !== undefined) && (op != undefined) && ()){
+                    // set n1 and calculate based on op
+                }
+            case 'Backspace': // what if operator deleted?
                 if(display_text.length == 0){
                     break;
                 }
@@ -90,6 +123,7 @@ buttons.addEventListener('click', (event) => {
                     update_text = display_text.slice(0, display_text.length - 1);
                     break;
                 }
+                // if + - * / power
             case '.': // 1 . per number not per display
                 if(display_text.includes('.')) {
                     break;
